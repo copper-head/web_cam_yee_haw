9e9b0bab4041d817e97e238c701296e7cf7aa537
copper-head <asslicker@welikeasses.com>
2022-03-19 16:18:38 -0500

    somethingssss

--- sender.py
+++ sender.py
@@ -7,7 +7,7 @@ class Sender:
 
     def __init__(self, remote_address):
 
-        self._socket = socket.create_connection(remote_address, constants.SOURCE_ADDRESS)
+        self._socket = socket.create_connection(remote_address, source_address=constants.SOURCE_ADDRESS)
         
 
     '''
